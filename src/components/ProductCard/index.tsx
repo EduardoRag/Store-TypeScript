366cced4cb0224bcb2b2f72bd80044809b592566
EduardoRag <eduardojarek66@gmail.com>
2023-06-28 15:12:12 -0300

    Adicionado botão de exclusão de produto no card

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,14 +1,33 @@
 import ProductType from '../../types/ProductType';
 import styles from './styles.module.scss';
+import RemoveIcon from '../../assets/remove.svg';
+import { useState } from 'react';
 
 type Props = {
     product: ProductType
 }
 
 function ProductCard({ product }: Props) {
+    const [showDelete, setShowDelete] = useState(false);
+
+
     return (
-        <div className={styles.container}>
+        <div
+            className={styles.container}
+            onMouseOver={() => setShowDelete(true)}
+            onMouseLeave={() => setShowDelete(false)}
+        >
+            {showDelete &&
+
+                <img
+                    className={styles.remove}
+                    src={RemoveIcon}
+                    alt='Remove Icon'
+                />
+            }
+
             <img
+                className={styles['card-image']}
                 src={product.image}
                 alt='product'
             />
